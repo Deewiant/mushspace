942813f8f26db7632cb5be349bd0e0d797813304
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-07 18:44:22 +0300

    Add mushspace_get, using only the static AABB now.

--- space_impl.h
+++ space_impl.h
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 
 #include "typenames.h"
-
+#include "coords.h"
 #include "stats.h"
 
 typedef struct mushspace mushspace;
 
+#define mushspace_get MUSHSPACE_CAT(mushspace,_get)
+
 size_t     MUSHSPACE_CAT(mushspace,_size)    (void);
 mushspace *MUSHSPACE_CAT(mushspace,_allocate)(void*, mush_stats*);
 void       MUSHSPACE_CAT(mushspace,_free)    (mushspace*);
+
+mushcell mushspace_get(mushspace*, mushcoords);
