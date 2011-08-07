60e498aa6936841de82d5fadd3cf2e453306a3b3
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-07 17:33:40 +0300

    Add mush_bakaabb_init.

---
+++ bakaabb.c
@@ -0,0 +1,6 @@
+// File created: 2011-08-07 17:31:02
+
+#pragma push_macro("INCLUDEE")
+#undef INCLUDEE
+#define INCLUDEE "bakaabb_impl.c.in"
+#include "includer.h"
--- bakaabb_impl.h
+++ bakaabb_impl.h
@@ -1,6 +1,15 @@
 // File created: 2011-08-06 17:55:19
 
+#include "lib/tommyds/tommyhashdyn.h"
+
 #include "typenames.h"
+#include "coords.h"
 
 typedef struct mush_bakaabb {
+	tommy_hashdyn data;
+	mushcoords beg, end;
 } mush_bakaabb;
+
+#define mush_bakaabb_init MUSHSPACE_CAT(mush_bakaabb,_init)
+
+void mush_bakaabb_init(mush_bakaabb*, mushcoords);
