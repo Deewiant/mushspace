2aa162fe807a9c3dcd7ff790cf6c2a50f17c47df
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-07 15:45:13 +0300

    Add the array to mush_staticaabb and memset it.

--- staticaabb.h
+++ staticaabb.h
@@ -3,6 +3,17 @@
 #ifndef MUSHSPACE_STATICAABB_H
 #define MUSHSPACE_STATICAABB_H
 
+#include "coords.h"
+
+// Need these separately for defining the static array.
+#define STATICAABB_SIZE_X 128
+#define STATICAABB_SIZE_Y 512
+#define STATICAABB_SIZE_Z   3
+
+#define STATICAABB_BEG  MUSHCOORDS( -16, -16, -1)
+#define STATICAABB_SIZE MUSHCOORDS(STATICAABB_SIZE_X, STATICAABB_SIZE_Y,\
+                                   STATICAABB_SIZE_Z)
+
 #pragma push_macro("INCLUDEE")
 #undef INCLUDEE
 #define INCLUDEE "staticaabb_impl.h"
--- staticaabb_impl.h
+++ staticaabb_impl.h
@@ -2,5 +2,16 @@
 
 #include "typenames.h"
 
+#include "coords.h"
+
 typedef struct mush_staticaabb {
+
+#if MUSHSPACE_DIM == 1
+		mushcell array[STATICAABB_SIZE_X];
+#elif MUSHSPACE_DIM == 2
+		mushcell array[STATICAABB_SIZE_X*STATICAABB_SIZE_Y];
+#elif MUSHSPACE_DIM == 3
+		mushcell array[STATICAABB_SIZE_X*STATICAABB_SIZE_Y*STATICAABB_SIZE_Z];
+#endif
+
 } mush_staticaabb;
