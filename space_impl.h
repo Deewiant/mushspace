ecae6979c40c968543db9293401e30fe25b3d5e1
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-09 11:55:24 +0300

    Add mushspace_load_string for Befunge-93.

--- space_impl.h
+++ space_impl.h
@@ -1,5 +1,6 @@
 // File created: 2011-08-06 15:58:49
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "coords.h"
@@ -17,3 +18,10 @@ mushspace *MUSHSPACE_CAT(mushspace,_allocate)(void*, mush_stats*);
 void       MUSHSPACE_CAT(mushspace,_free)    (mushspace*);
 
 mushcell mushspace_get(mushspace*, mushcoords);
+
+void MUSHSPACE_CAT(mushspace,_load_string)
+	( mushspace*, const char*, size_t len
+#if !MUSHSPACE_93
+	, mushcoords* end, mushcoords target, bool binary
+#endif
+	);
