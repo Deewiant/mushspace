82b1a0bcaeb1626604a277025f5142cfa9e0a513
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-10 14:16:42 +0300

    Change the way stats are used, add mushstats_add.

---
+++ stats.c
@@ -0,0 +1,14 @@
+// File created: 2011-08-09 19:15:00
+
+#include "stats.h"
+
+void mushstats_add(mushstats* stats, MushStat stat, uint64_t val) {
+#ifdef ENABLE_STATS
+	switch (stat) {
+	case MushStat_lookups:     stats->lookups     += val; break;
+	case MushStat_assignments: stats->assignments += val; break;
+	}
+#else
+	(void)stats; (void)stat; (void)val;
+#endif
+}
--- stats.h
+++ stats.h
@@ -5,8 +5,22 @@
 
 #include <stdint.h>
 
+#define ENABLE_STATS
+
 typedef struct mushstats {
+#ifdef ENABLE_STATS
 	uint64_t lookups;
+	uint64_t assignments;
+#else
+	char unused;
+#endif
 } mushstats;
+
+typedef enum {
+	MushStat_lookups,
+	MushStat_assignments,
+} MushStat;
+
+void mushstats_add(mushstats*, MushStat, uint64_t);
 
 #endif
