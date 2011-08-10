180e4f15d0ad67785f46608ff04eaface26bbc93
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-10 15:33:14 +0300

    Have mushspace_load_string return an error code.

---
+++ errors.h
@@ -0,0 +1,11 @@
+// File created: 2011-08-10 13:40:12
+
+#ifndef MUSHSPACE_ERRORS_H
+#define MUSHSPACE_ERRORS_H
+
+enum {
+	MUSH_ERR_OOM     = 1,
+	MUSH_ERR_NO_ROOM = 2,
+};
+
+#endif
--- space_impl.h
+++ space_impl.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include "coords.h"
+#include "errors.h"
 #include "stats.h"
 #include "typenames.h"
 
@@ -19,7 +20,14 @@ void       MUSHSPACE_CAT(mushspace,_free)    (mushspace*);
 
 mushcell mushspace_get(mushspace*, mushcoords);
 
-void MUSHSPACE_CAT(mushspace,_load_string)
+// Returns 0 on success or one of the following possible error codes:
+//
+// MUSH_ERR_OOM:     Ran out of memory somewhere.
+// MUSH_ERR_NO_ROOM: The string doesn't fit in the space, i.e. it would overlap
+//                   with itself. For instance, trying to binary-load 5
+//                   gigabytes of non-space data into a 32-bit space would
+//                   cause this error.
+int MUSHSPACE_CAT(mushspace,_load_string)
 	( mushspace*, const char*, size_t len
 #if !MUSHSPACE_93
 	, mushcoords* end, mushcoords target, bool binary
