18d261445cca9c5b6b723adf58d23e05a84282e3
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-10 15:27:32 +0300

    Add stdlib.h, containing only MUSH_ARRAY_LEN now.

---
+++ stdlib.h
@@ -0,0 +1,10 @@
+// File created: 2011-08-09 19:39:28
+
+#ifndef MUSHSPACE_STDLIB_H
+#define MUSHSPACE_STDLIB_H
+
+#include <stdlib.h>
+
+#define MUSH_ARRAY_LEN(X) (sizeof(X) / sizeof((X)[0]))
+
+#endif
