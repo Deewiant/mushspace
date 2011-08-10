659cf78a1de83a988f31700111034c6f0a15dbad
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-10 15:27:56 +0300

    Add mush_size_t_max.

---
+++ stdlib.c
@@ -0,0 +1,5 @@
+// File created: 2011-08-09 19:51:08
+
+#include "stdlib.h"
+
+size_t mush_size_t_max(size_t a, size_t b) { return a > b ? a : b; }
--- stdlib.h
+++ stdlib.h
@@ -7,4 +7,6 @@
 
 #define MUSH_ARRAY_LEN(X) (sizeof(X) / sizeof((X)[0]))
 
+size_t mush_size_t_max(size_t, size_t);
+
 #endif
