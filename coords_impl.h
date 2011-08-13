f1ccdab358775521594b9fca99f2b6759b751052
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-13 20:31:30 +0300

    Add mushcoords_{add,sub}s_clamped.

--- coords_impl.h
+++ coords_impl.h
@@ -30,8 +30,11 @@ typedef union mushcoords {
 
 #define mushcoords_contains MUSHSPACE_CAT(mushcoords,_contains)
 
-#define mushcoords_sub   MUSHSPACE_CAT(mushcoords,_sub)
-#define mushcoords_equal MUSHSPACE_CAT(mushcoords,_equal)
+#define mushcoords_sub         MUSHSPACE_CAT(mushcoords,_sub)
+#define mushcoords_equal       MUSHSPACE_CAT(mushcoords,_equal)
+
+#define mushcoords_adds_clamped MUSHSPACE_CAT(mushcoords,_adds_clamped)
+#define mushcoords_subs_clamped MUSHSPACE_CAT(mushcoords,_subs_clamped)
 
 #define mushcoords_max_into MUSHSPACE_CAT(mushcoords,_max_into)
 
@@ -40,8 +43,12 @@ typedef union mushcoords {
 
 bool mushcoords_contains(mushcoords pos, mushcoords beg, mushcoords end);
 
-mushcoords mushcoords_sub  (mushcoords, mushcoords);
-bool       mushcoords_equal(mushcoords, mushcoords);
+mushcoords mushcoords_sub(mushcoords, mushcoords);
+
+mushcoords mushcoords_adds_clamped(mushcoords, mushcell);
+mushcoords mushcoords_subs_clamped(mushcoords, mushcell);
+
+bool mushcoords_equal(mushcoords, mushcoords);
 
 void mushcoords_max_into(mushcoords*, mushcoords);
 
