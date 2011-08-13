f78da2cc784aa982f5bd069ac397875b8652461f
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-13 20:50:02 +0300

    Add mush_anamnesic_ring_{push,read,last}.

--- anamnesic_ring_impl.h
+++ anamnesic_ring_impl.h
@@ -24,5 +24,13 @@ typedef struct mush_anamnesic_ring {
 } mush_anamnesic_ring;
 
 #define mush_anamnesic_ring_init MUSHSPACE_CAT(mush_anamnesic_ring,_init)
+#define mush_anamnesic_ring_push MUSHSPACE_CAT(mush_anamnesic_ring,_push)
+#define mush_anamnesic_ring_read MUSHSPACE_CAT(mush_anamnesic_ring,_read)
+#define mush_anamnesic_ring_last MUSHSPACE_CAT(mush_anamnesic_ring,_last)
 
 void mush_anamnesic_ring_init(mush_anamnesic_ring*);
+
+void mush_anamnesic_ring_push(mush_anamnesic_ring*, mush_memory);
+
+void mush_anamnesic_ring_read(const mush_anamnesic_ring*, mush_memory*);
+const mush_memory* mush_anamnesic_ring_last(const mush_anamnesic_ring*);
