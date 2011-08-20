2885e319295a906ee9863f718d1aec2122414a5f
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-20 12:50:07 +0300

    aabb: reorder functions in files.

--- aabb_impl.h
+++ aabb_impl.h
@@ -28,6 +28,7 @@ typedef struct mush_aabb {
 #define mush_aabb_volume_on MUSHSPACE_CAT(mush_aabb,_volume_on)
 
 #define mush_aabb_contains      MUSHSPACE_CAT(mush_aabb,_contains)
+#define mush_aabb_contains_box  MUSHSPACE_CAT(mush_aabb,_contains_box)
 #define mush_aabb_safe_contains MUSHSPACE_CAT(mush_aabb,_safe_contains)
 
 #define mush_aabb_get MUSHSPACE_CAT(mush_aabb,_get)
@@ -36,9 +37,8 @@ typedef struct mush_aabb {
 #define mush_aabb_get_idx           MUSHSPACE_CAT(mush_aabb,_get_idx)
 #define mush_aabb_get_idx_no_offset MUSHSPACE_CAT(mush_aabb,_get_idx_no_offset)
 
-#define mush_aabb_overlaps     MUSHSPACE_CAT(mush_aabb,_overlaps)
-#define mush_aabb_overlapsc    MUSHSPACE_CAT(mush_aabb,_overlapsc)
-#define mush_aabb_contains_box MUSHSPACE_CAT(mush_aabb,_contains_box)
+#define mush_aabb_overlaps  MUSHSPACE_CAT(mush_aabb,_overlaps)
+#define mush_aabb_overlapsc MUSHSPACE_CAT(mush_aabb,_overlapsc)
 
 #define mush_aabb_tessellate  MUSHSPACE_CAT(mush_aabb,_tessellate)
 #define mush_aabb_tessellate1 MUSHSPACE_CAT(mush_aabb,_tessellate1)
@@ -50,7 +50,8 @@ bool mush_aabb_alloc      (mush_aabb*);
 
 size_t mush_aabb_volume_on(const mush_aabb*, mushdim axis);
 
-bool mush_aabb_contains(const mush_aabb*, mushcoords);
+bool mush_aabb_contains    (const mush_aabb*, mushcoords);
+bool mush_aabb_contains_box(const mush_aabb*, const mush_aabb*);
 
 // Works even if the AABB is an unsafe box with beg > end.
 bool mush_aabb_safe_contains(const mush_aabb*, mushcoords);
@@ -61,9 +62,8 @@ void     mush_aabb_put(      mush_aabb*, mushcoords, mushcell);
 size_t mush_aabb_get_idx          (const mush_aabb*, mushcoords);
 size_t mush_aabb_get_idx_no_offset(const mush_aabb*, mushcoords);
 
-bool mush_aabb_overlaps    (const mush_aabb*, const mush_aabb*);
-bool mush_aabb_overlapsc   (const mush_aabb*, mushcoords, mushcoords);
-bool mush_aabb_contains_box(const mush_aabb*, const mush_aabb*);
+bool mush_aabb_overlaps (const mush_aabb*, const mush_aabb*);
+bool mush_aabb_overlapsc(const mush_aabb*, mushcoords, mushcoords);
 
 void mush_aabb_tessellate(
 	mushcoords, const mush_aabb*, size_t, mushcoords*, mushcoords*);
