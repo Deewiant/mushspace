1e80813406e4bedc68aaedddb77a448846924731
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-08-21 18:18:15 +0300

    Add mush_bounds and port several aabb funcs to it.

---
+++ bounds.all.c
@@ -0,0 +1,157 @@
+// File created: 2011-08-21 16:17:09
+
+#include "bounds.all.h"
+
+#include <assert.h>
+
+#include "stdlib.any.h"
+
+size_t mush_bounds_clamped_size(const mush_bounds* bounds) {
+	size_t sz = 1;
+	for (mushdim i = 0; i < MUSHSPACE_DIM; ++i)
+		sz = mush_size_t_mul_clamped(
+			sz, mush_size_t_add_clamped(bounds->end.v[i] - bounds->beg.v[i], 1));
+	return sz;
+}
+
+bool mush_bounds_contains(const mush_bounds* bounds, mushcoords pos) {
+	if (pos.x < bounds->beg.x || pos.x > bounds->end.x) return false;
+#if MUSHSPACE_DIM >= 2
+	if (pos.y < bounds->beg.y || pos.y > bounds->end.y) return false;
+#if MUSHSPACE_DIM >= 3
+	if (pos.z < bounds->beg.z || pos.z > bounds->end.z) return false;
+#endif
+#endif
+	return true;
+}
+bool mush_bounds_safe_contains(const mush_bounds* bounds, mushcoords pos) {
+	for (mushdim i = 0; i < MUSHSPACE_DIM; ++i) {
+		if (bounds->beg.v[i] > bounds->end.v[i]) {
+			if (!(pos.v[i] >= bounds->beg.v[i] || pos.v[i] <= bounds->end.v[i]))
+				return false;
+		} else {
+			if (!(pos.v[i] >= bounds->beg.v[i] && pos.v[i] <= bounds->end.v[i]))
+				return false;
+		}
+	}
+	return true;
+}
+
+bool mush_bounds_contains_bounds(const mush_bounds* a, const mush_bounds* b) {
+	return mush_bounds_contains(a, b->beg) && mush_bounds_contains(a, b->end);
+}
+bool mush_bounds_overlaps(const mush_bounds* a, const mush_bounds* b) {
+	for (mushdim i = 0; i < MUSHSPACE_DIM; ++i)
+		if (a->beg.v[i] > b->end.v[i] || b->beg.v[i] > a->end.v[i])
+			return false;
+	return true;
+}
+
+#if !MUSHSPACE_93
+bool mush_bounds_get_overlap(
+	const mush_bounds* a, const mush_bounds* b, mush_bounds* overlap)
+{
+	if (!mush_bounds_overlaps(a, b))
+		return false;
+
+	overlap->beg = a->beg; mushcoords_max_into(&overlap->beg, b->beg);
+	overlap->end = a->end; mushcoords_min_into(&overlap->end, b->end);
+
+	assert (mush_bounds_contains_bounds(a, overlap));
+	assert (mush_bounds_contains_bounds(b, overlap));
+	return true;
+}
+#endif
+
+#if MUSHSPACE_DIM > 1
+bool mush_bounds_on_same_axis(const mush_bounds* a, const mush_bounds* b) {
+	for (mushdim i = 0; i < MUSHSPACE_DIM; ++i)
+		if (a->beg.v[i] == b->beg.v[i] && a->end.v[i] == b->end.v[i])
+			return true;
+	return false;
+}
+bool mush_bounds_on_same_primary_axis(
+	const mush_bounds* a, const mush_bounds* b)
+{
+	const mushdim I = MUSHSPACE_DIM-1;
+	return a->beg.v[I] == b->beg.v[I] && a->end.v[I] == b->end.v[I];
+}
+#endif
+
+#if !MUSHSPACE_93
+bool mush_bounds_can_fuse(const mush_bounds* a, const mush_bounds* b) {
+	bool overlap = mush_bounds_overlaps(a, b);
+
+	for (mushdim i = 0; i < MUSHSPACE_DIM; ++i) {
+		if (a->beg.v[i] == b->beg.v[i] && a->end.v[i] == b->end.v[i])
+			continue;
+
+		if (!(   overlap
+		      || mushcell_add_clamped(a->end.v[i], 1) == b->beg.v[i]
+		      || mushcell_add_clamped(b->end.v[i], 1) == a->beg.v[i]))
+			return false;
+
+		for (mushdim j = i+1; j < MUSHSPACE_DIM; ++j)
+			if (a->beg.v[j] != b->beg.v[j] || a->end.v[j] != b->end.v[j])
+				return false;
+
+		#if MUSHSPACE_DIM > 1
+			assert (mush_bounds_on_same_axis(a, b));
+		#endif
+		return true;
+	}
+	return false;
+}
+#endif
+
+void mush_bounds_tessellate(
+	mushcoords pos, const mush_bounds* bs, size_t len, mush_bounds* bounds)
+{
+	assert (mush_bounds_contains(bounds, pos));
+
+	for (size_t i = 0; i < len; ++i)
+		mush_bounds_tessellate1(pos, &bs[i], bounds);
+}
+void mush_bounds_tessellate1(
+	mushcoords pos, const mush_bounds* avoid, mush_bounds* bounds)
+{
+	assert (mush_bounds_contains(bounds, pos));
+
+	for (mushdim i = 0; i < MUSHSPACE_DIM; ++i) {
+		// This could be improved, consider for instance the bottommost box in
+		// the following graphic and its current tessellation:
+		//
+		// +-------+    +--*--*-+
+		// |       |    |X .  . |
+		// |       |    |  .  . |
+		// |     +---   *..*..+---
+		// |     |      |  .  |
+		// |  +--|      *..+--|
+		// |  |  |      |  |  |
+		// |  |  |      |  |  |
+		// +--|  |      +--|  |
+		//
+		// (Note that this isn't actually a tessellation: all points will get
+		// a rectangle containing the rectangle at X.)
+		//
+		// Any of the following three would be an improvement (and they would
+		// actually be tessellations):
+		//
+		// +--*--*-+    +-------+    +-----*-+
+		// |  .  . |    |       |    |     . |
+		// |  .  . |    |       |    |     . |
+		// |  .  +---   *.....+---   |     +---
+		// |  .  |      |     |      |     |
+		// |  +--|      *..+--|      *..+--|
+		// |  |  |      |  |  |      |  |  |
+		// |  |  |      |  |  |      |  |  |
+		// +--|  |      +--|  |      +--|  |
+
+		mushcell ab = avoid->beg.v[i],
+		         ae = avoid->end.v[i],
+		         p  = pos.v[i];
+		if (ae < p) bounds->beg.v[i] = mushcell_max(bounds->beg.v[i], ae+1);
+		if (ab > p) bounds->end.v[i] = mushcell_min(bounds->end.v[i], ab-1);
+	}
+	assert (!mush_bounds_overlaps(bounds, avoid));
+}
---
+++ bounds.all.h
@@ -0,0 +1,63 @@
+// File created: 2011-08-21 16:17:06
+
+#ifndef MUSHSPACE_BOUNDS_H
+#define MUSHSPACE_BOUNDS_H
+
+#include "coords.all.h"
+#include "typenames.any.h"
+
+#define mush_bounds MUSHSPACE_NAME(mush_bounds)
+
+typedef struct mush_bounds {
+	mushcoords beg, end;
+} mush_bounds;
+
+#define mush_bounds_clamped_size    MUSHSPACE_CAT(mush_bounds,_clamped_size)
+#define mush_bounds_contains        MUSHSPACE_CAT(mush_bounds,_contains)
+#define mush_bounds_safe_contains   MUSHSPACE_CAT(mush_bounds,_safe_contains)
+#define mush_bounds_contains_bounds MUSHSPACE_CAT(mush_bounds,_contains_bounds)
+#define mush_bounds_overlaps        MUSHSPACE_CAT(mush_bounds,_overlaps)
+#define mush_bounds_get_overlap     MUSHSPACE_CAT(mush_bounds,_get_overlap)
+#define mush_bounds_on_same_axis    MUSHSPACE_CAT(mush_bounds,_on_same_axis)
+#define mush_bounds_can_fuse        MUSHSPACE_CAT(mush_bounds,_can_fuse)
+#define mush_bounds_tessellate      MUSHSPACE_CAT(mush_bounds,_tessellate)
+#define mush_bounds_tessellate1     MUSHSPACE_CAT(mush_bounds,_tessellate1)
+
+#define mush_bounds_on_same_primary_axis \
+	MUSHSPACE_CAT(mush_bounds,_on_same_primary_axis)
+
+size_t mush_bounds_clamped_size(const mush_bounds*);
+
+// The safe version works even if the bounds have beg > end.
+bool mush_bounds_contains     (const mush_bounds*, mushcoords);
+bool mush_bounds_safe_contains(const mush_bounds*, mushcoords);
+
+bool mush_bounds_contains_bounds(const mush_bounds*, const mush_bounds*);
+bool mush_bounds_overlaps       (const mush_bounds*, const mush_bounds*);
+
+#if !MUSHSPACE_93
+bool mush_bounds_get_overlap(
+	const mush_bounds*, const mush_bounds*, mush_bounds*);
+#endif
+
+#if MUSHSPACE_DIM > 1
+bool mush_bounds_on_same_axis        (const mush_bounds*, const mush_bounds*);
+bool mush_bounds_on_same_primary_axis(const mush_bounds*, const mush_bounds*);
+#endif
+
+#if !MUSHSPACE_93
+// True if we can create a new AABB which covers exactly the two bounds given.
+bool mush_bounds_can_fuse(const mush_bounds*, const mush_bounds*);
+#endif
+
+// Modifies the given bounds to be such that they contain the given coordinates
+// but don't overlap with any of the given boxes. The coordinates should, of
+// course, be already contained between the beg and end.
+void mush_bounds_tessellate(
+	mushcoords, const mush_bounds*, size_t, mush_bounds*);
+
+// Since the algorithm is currently just a fold over the boxes, this simpler
+// version exists to avoid heap allocation in some cases.
+void mush_bounds_tessellate1(mushcoords, const mush_bounds*, mush_bounds*);
+
+#endif
