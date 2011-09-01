3ef77b01ebb7d2ecf6c187ef8ebc95d93cfce6d6
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2011-09-01 23:50:16 +0300

    Add mush_bakaabb_iter_sizeof.

--- bakaabb.98.h
+++ bakaabb.98.h
@@ -19,18 +19,21 @@ typedef struct mush_bakaabb {
 
 typedef struct mush_bakaabb_iter mush_bakaabb_iter;
 
-#define mush_bakaabb_init      MUSHSPACE_CAT(mush_bakaabb,_init)
-#define mush_bakaabb_free      MUSHSPACE_CAT(mush_bakaabb,_free)
-#define mush_bakaabb_get       MUSHSPACE_CAT(mush_bakaabb,_get)
-#define mush_bakaabb_put       MUSHSPACE_CAT(mush_bakaabb,_put)
-#define mush_bakaabb_size      MUSHSPACE_CAT(mush_bakaabb,_size)
-#define mush_bakaabb_it_start  MUSHSPACE_CAT(mush_bakaabb,_it_start)
-#define mush_bakaabb_it_stop   MUSHSPACE_CAT(mush_bakaabb,_it_stop)
-#define mush_bakaabb_it_done   MUSHSPACE_CAT(mush_bakaabb,_it_done)
-#define mush_bakaabb_it_next   MUSHSPACE_CAT(mush_bakaabb,_it_next)
-#define mush_bakaabb_it_remove MUSHSPACE_CAT(mush_bakaabb,_it_remove)
-#define mush_bakaabb_it_pos    MUSHSPACE_CAT(mush_bakaabb,_it_pos)
-#define mush_bakaabb_it_val    MUSHSPACE_CAT(mush_bakaabb,_it_val)
+#define mush_bakaabb_iter_sizeof MUSHSPACE_CAT(mush_bakaabb_iter,_sizeof)
+#define mush_bakaabb_init        MUSHSPACE_CAT(mush_bakaabb,_init)
+#define mush_bakaabb_free        MUSHSPACE_CAT(mush_bakaabb,_free)
+#define mush_bakaabb_get         MUSHSPACE_CAT(mush_bakaabb,_get)
+#define mush_bakaabb_put         MUSHSPACE_CAT(mush_bakaabb,_put)
+#define mush_bakaabb_size        MUSHSPACE_CAT(mush_bakaabb,_size)
+#define mush_bakaabb_it_start    MUSHSPACE_CAT(mush_bakaabb,_it_start)
+#define mush_bakaabb_it_stop     MUSHSPACE_CAT(mush_bakaabb,_it_stop)
+#define mush_bakaabb_it_done     MUSHSPACE_CAT(mush_bakaabb,_it_done)
+#define mush_bakaabb_it_next     MUSHSPACE_CAT(mush_bakaabb,_it_next)
+#define mush_bakaabb_it_remove   MUSHSPACE_CAT(mush_bakaabb,_it_remove)
+#define mush_bakaabb_it_pos      MUSHSPACE_CAT(mush_bakaabb,_it_pos)
+#define mush_bakaabb_it_val      MUSHSPACE_CAT(mush_bakaabb,_it_val)
+
+extern const size_t mush_bakaabb_iter_sizeof;
 
 bool mush_bakaabb_init(mush_bakaabb*, mushcoords);
 void mush_bakaabb_free(mush_bakaabb* bak);
