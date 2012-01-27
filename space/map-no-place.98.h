1ae79086a042b45152f8be4ff2b100fe659eb1f9
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2012-01-27 21:52:29 +0200

    mushspace_map* -> space/map-no-place.98.c

---
+++ space/map-no-place.98.h
@@ -0,0 +1,26 @@
+// File created: 2012-01-27 20:59:31
+
+#ifndef MUSHSPACE_SPACE_MAP_NO_PLACE_H
+#define MUSHSPACE_SPACE_MAP_NO_PLACE_H
+
+#include "space.all.h"
+#include "types.h"
+
+#define mushspace_map_no_place   MUSHSPACE_CAT(mushspace,_map_no_place)
+#define mushspace_mapex_no_place MUSHSPACE_CAT(mushspace,_mapex_no_place)
+
+typedef struct mush_bounded_pos {
+	const mush_bounds *bounds;
+	mushcoords *pos;
+} mush_bounded_pos;
+
+void mushspace_map_no_place(
+	mushspace*, const mush_aabb*, void*,
+	void(*)(mush_arr_mushcell, void*), void(*)(size_t, void*));
+
+void mushspace_mapex_no_place(
+	mushspace*, const mush_aabb*, void*,
+	void(*)(mush_arr_mushcell, void*, size_t,size_t, size_t,size_t, uint8_t*),
+	void(*)(size_t, void*));
+
+#endif
