28b5830a3697c170a36b4dd0fc2027041eed4c7b
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2012-01-28 00:07:14 +0200

    mushspace_place_box_for -> space/place-box-for.*

---
+++ space/place-box-for.98.h
@@ -0,0 +1,12 @@
+// File created: 2012-01-28 00:00:33
+
+#ifndef MUSHSPACE_SPACE_PLACE_BOX_FOR_H
+#define MUSHSPACE_SPACE_PLACE_BOX_FOR_H
+
+#include "space.all.h"
+
+#define mushspace_place_box_for MUSHSPACE_CAT(mushspace,_place_box_for)
+
+bool mushspace_place_box_for(mushspace*, mushcoords, mush_aabb**);
+
+#endif
