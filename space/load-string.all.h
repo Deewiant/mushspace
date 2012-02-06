441ee43c449ff32d554aab1a5ba5f4375af62503
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2012-02-06 17:59:51 +0200

    space/load-string: export UTF/mushcell versions

--- space/load-string.all.h
+++ space/load-string.all.h
@@ -5,7 +5,10 @@
 
 #include "space.all.h"
 
-#define mushspace_load_string MUSHSPACE_CAT(mushspace,_load_string)
+#define mushspace_load_string       MUSHSPACE_CAT(mushspace,_load_string)
+#define mushspace_load_string_utf8  MUSHSPACE_CAT(mushspace,_load_string_utf8)
+#define mushspace_load_string_utf16 MUSHSPACE_CAT(mushspace,_load_string_utf16)
+#define mushspace_load_string_cell  MUSHSPACE_CAT(mushspace,_load_string_cell)
 
 // Returns 0 on success or one of the following possible error codes:
 //
@@ -16,6 +19,27 @@
 //                  cause this error.
 int mushspace_load_string
    ( mushspace*, const char*, size_t len
+#ifndef MUSHSPACE_93
+   , mushcoords* end, mushcoords target, bool binary
+#endif
+   );
+
+int mushspace_load_string_utf8
+   ( mushspace*, const uint8_t*, size_t len
+#ifndef MUSHSPACE_93
+   , mushcoords* end, mushcoords target, bool binary
+#endif
+   );
+
+int mushspace_load_string_utf16
+   ( mushspace*, const uint16_t*, size_t len
+#ifndef MUSHSPACE_93
+   , mushcoords* end, mushcoords target, bool binary
+#endif
+   );
+
+int mushspace_load_string_cell
+   ( mushspace*, const mushcell*, size_t len
 #ifndef MUSHSPACE_93
    , mushcoords* end, mushcoords target, bool binary
 #endif
