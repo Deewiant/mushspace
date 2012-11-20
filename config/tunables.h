225bd4403c76f99f6a9e7642b1a6a0ef5501db81
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2012-11-20 16:47:41 +0200

    config/tunables: doc BOXEN_* and USE_BAKAABB

--- config/tunables.h
+++ config/tunables.h
@@ -3,9 +3,14 @@
 #ifndef MUSH_CONFIG_TUNABLES_H
 #define MUSH_CONFIG_TUNABLES_H
 
+// Which mushboxen implementation to use. Define only one, please!
 //#define BOXEN_ARRAY
 #define BOXEN_RTREE
 
+// Whether to use mushbakaabb or not. Note that it's useless for -93, since
+// that uses only the static box in any case.
+//
+// Currently only used for arrays, as it doesn't benefit anything else.
 #define USE_BAKAABB (!MUSHSPACE_93 && defined(BOXEN_ARRAY))
 
 #if USE_BAKAABB
